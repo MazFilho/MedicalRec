199f415588d9bf1b15048c4464164e2b39ceb3cf
Osmar Rodrigues <mazinho_skt42@hotmail.com>
2021-02-27 22:37:32 -0300

    Adicionar arquivos de projeto.

---
+++ MedicalRec.Dominio/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MedicalRec.Dominio
+{
+    public class Class1
+    {
+    }
+}
---
+++ MedicalRec.Repositorio/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MedicalRec.Repositorio
+{
+    public class Class1
+    {
+    }
+}
