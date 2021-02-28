bce58c308487d07121093ee70fe8424ebda07432
Osmar Rodrigues <mazinho_skt42@hotmail.com>
2021-02-28 16:51:11 -0300

    Criação do DbContext e configuração dos DbSet

---
+++ MedicalRec.Repositorio/Contexto/MedicalRecContexto.cs
@@ -0,0 +1,13 @@
+﻿using MedicalRec.Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedicalRec.Repositorio.Contexto
+{
+    public class MedicalRecContexto : DbContext
+    {
+        public DbSet<Medico> Medicos { get; set; }
+        public DbSet<Especialidade> Especialidades { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+
+    }
+}
